078423f59451f07faf0b61fbf400ce34691e7377
cata <catalin.rscata@gmail.com>
2018-10-16 15:33:50 +0300

    mustache

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,35 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
-    var li = jQuery('<li></li>');
-    li.text(message.from + ':' + message.text);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#message').append(li);
+    jQuery('#message').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#message').append(html);
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
+
+    // li.text(`${message.from} ${formattedTime} : `);
+    // a.attr('href', message.url);
     
-    li.append(a);
-    jQuery('#message').append(li);
+    // li.append(a);
+    // jQuery('#message').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e) {
